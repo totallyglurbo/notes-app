d05dd187bdf50e30476199b482ebddc8a48c0185
totallyglurbo <glurb@glurbin.com>
2026-02-19 00:56:13 +0700

    Add how cards are displayed Added conditions for the first column and a correct way to display the cards

--- js/main.js
+++ js/main.js
@@ -10,11 +10,14 @@ Vue.component('fst-column', {
             <h2>Cards</h2>
             <p v-if="!cards.length">There are no cards yet.</p>
             <ul>
-              <li v-for="card in cards">
-              <p>{{ card.name }}</p>
+              <li v-for="(card, index) in cards" :key="index">
+                <p><strong>{{ card.name }}</strong></p>
+                <ul>
+                  <li v-for="(option, index) in card.options" :key="index">{{ option }}</li>
+                </ul>
               </li>
             </ul>
-           </div> <card @card-submitted="addCard"></card>
+           </div> <card v-if="cards.length < 3"@card-submitted="addCard"></card>
        </div>
 
     </div>
@@ -52,15 +55,14 @@ Vue.component('card', {
          <p>
            <label for="name">Name:</label>
            <input id="name" v-model="name" placeholder="Name">
-           <label for="itemCount">Number of options:</label>
-           <select id="options" v-model.number="itemCount">
-            <option v-for="option in options" :key="option" :value="option">{{ option }}</option>
-           </select>
          </p>
-         <p>
-           <label for="options">Options:</label>
-           <input id="options" v-model="options" placeholder="Options">
-         </p>
+         <div>
+          <input v-model="newItem" placeholder="Введите пункт" />
+          <button @click.prevent="addItem" :disabled="listItems.length >= 5">Добавить пункт</button>
+        </div>
+         <ul>
+          <li v-for="(item, index) in listItems" :key="index">{{ item }}</li>
+        </ul>
          <p>
            <input type="submit" value="Submit"> 
          </p>
@@ -69,8 +71,7 @@ Vue.component('card', {
     data() {
         return {
             name: null,
-            options: [3, 4, 5],
-            itemCount: 3,
+            options: [],
             newItem: null,
             listItems: [],
             errors: []
@@ -79,26 +80,28 @@ Vue.component('card', {
 
     methods:{
         addItem() {
-            if (this.newItem && this.listItems.length < this.itemCount) {
+            if (this.newItem && this.listItems.length < 5) {
                 this.listItems.push(this.newItem);
                 this.newItem = null;
             }
         },
         onSubmit() {
-            if(this.name) {
+            this.errors = [];
+            if (!this.name) {
+                this.errors.push("Name required.");
+            }
+            if (this.listItems.length < 3) {
+                this.errors.push("Please enter at least 3 options.");
+            }
+            if (this.errors.length === 0) {
                 let cardItem = {
                     name: this.name,
-                    options: this.options
-                }
-                this.$emit('card-submitted', cardItem)
+                    options: this.listItems
+                };
+                this.$emit('card-submitted', cardItem);
                 this.name = null;
                 this.listItems = [];
-                this.itemCount = 3;
-            } else {
-                if(!this.name) this.errors.push("Name required.")
-                if(this.listItems.length === 0) {
-                    this.errors.push("One option required.")
-                }
+                this.newItem = null;
             }
         }
     }

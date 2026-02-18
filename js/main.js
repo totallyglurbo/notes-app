b4e7d32608bee185d03e7ea7792f9d9e38357a42
totallyglurbo <glurb@glurbin.com>
2026-02-18 12:06:48 +0700

    Initial commit Added main components, created the main directory

---
+++ js/main.js
@@ -0,0 +1,29 @@
+let app = new Vue({
+    el: '#app',
+})
+Vue.component('fst-column', {
+    template: `
+    <div>
+        <h2>First Column</h2>
+    </div>
+    `
+})
+Vue.component('scnd-column', {
+    template: `
+    <div>
+        <h2>First Column</h2>
+    </div>
+    `
+})
+Vue.component('thd-column', {
+    template: `
+    <div>
+        <h2>First Column</h2>
+    </div>
+    `
+})
+Vue.component('card', {
+    template: `
+    <div></div>
+    `
+})

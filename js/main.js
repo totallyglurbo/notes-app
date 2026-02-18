2aa52fdd839974444c01b10b370d96596cf726f1
totallyglurbo <glurb@glurbin.com>
2026-02-18 13:42:49 +0700

    Add a card component Added another component for cards, started working on how it will be displayed

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
-let app = new Vue({
-    el: '#app',
-})
 Vue.component('fst-column', {
+    props: {
+        cards: Array,
+        max: Number
+    },
     template: `
     <div>
         <h2>First Column</h2>
@@ -11,14 +12,14 @@ Vue.component('fst-column', {
 Vue.component('scnd-column', {
     template: `
     <div>
-        <h2>First Column</h2>
+        <h2>Second Column</h2>
     </div>
     `
 })
 Vue.component('thd-column', {
     template: `
     <div>
-        <h2>First Column</h2>
+        <h2>Third Column</h2>
     </div>
     `
 })
@@ -26,4 +27,14 @@ Vue.component('card', {
     template: `
     <div></div>
     `
+})
+let app = new Vue({
+    el: '#app',
+    data: {
+        fstMax: 3;
+        scndMax: 5;
+        fstColumnCards: [],
+        scndColumnCards: [],
+        thdColumnCards: []
+    }
 })

515cea6c46099796683dfeac53d4ed120f24bb8c
totallyglurbo <glurb@glurbin.com>
2026-02-18 15:52:07 +0700

    Add option to create a card Added an option to create a card in the first column

--- js/main.js
+++ js/main.js
@@ -1,13 +1,30 @@
 Vue.component('fst-column', {
     props: {
-        cards: Array,
+        cards: [],
         max: Number
     },
     template: `
     <div>
         <h2>First Column</h2>
+        <div>
+            <h2>Cards</h2>
+            <p v-if="!cards.length">There are no cards yet.</p>
+            <ul>
+              <li v-for="card in cards">
+              <p>{{ card.name }}</p>
+              </li>
+            </ul>
+           </div> <card @card-submitted="addCard"></card>
+       </div>
+
     </div>
-    `
+    `,
+    methods: {
+        addCard(cardItem) {
+            this.cards.push(cardItem)
+        }
+
+    }
 })
 Vue.component('scnd-column', {
     template: `
@@ -25,14 +42,49 @@ Vue.component('thd-column', {
 })
 Vue.component('card', {
     template: `
-    <div></div>
-    `
+    <form class="review-form" @submit.prevent="onSubmit">
+        <p v-if="errors.length">
+            <b>Please correct the following error(s):</b>
+            <ul>
+                <li v-for="error in errors">{{ error }}</li>
+            </ul>
+        </p>
+         <p>
+           <label for="name">Name:</label>
+           <input id="name" v-model="name" placeholder="Name">
+         </p>
+         <p>
+           <input type="submit" value="Submit"> 
+         </p>
+    </form>
+    `,
+    data() {
+        return {
+            name: null,
+            errors: []
+        }
+    },
+
+    methods:{
+        onSubmit() {
+            if(this.name) {
+                let cardItem = {
+                    name: this.name,
+                }
+                this.$emit('card-submitted', cardItem)
+                this.name = null
+            } else {
+                if(!this.name) this.errors.push("Name required.")
+            }
+        }
+    }
+
 })
 let app = new Vue({
     el: '#app',
     data: {
-        fstMax: 3;
-        scndMax: 5;
+        fstMax: 3,
+        scndMax: 5,
         fstColumnCards: [],
         scndColumnCards: [],
         thdColumnCards: []
